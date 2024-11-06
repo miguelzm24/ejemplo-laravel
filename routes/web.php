6eaf1506b512e8d4a08777915a2db78d00e0b79f
miguelzm24 <mgilav@zubirimanteo.com>
2024-11-06 08:43:58 +0100

    Cambios en las rutas

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
-Route::get('/peliculas/create', [PeliculaController::class, 'create']);
+//Route::get('/peliculas/create', [PeliculaController::class, 'create']);
 Route::get('/peliculas/list', [PeliculaController::class, 'list']);
 Route::get('/peliculas/show/{id}', [PeliculaController::class, 'show']);
 Route::get('/peliculas/update', [PeliculaController::class, 'update']);
 Route::get('/peliculas/delete', [PeliculaController::class, 'delete']);
 
 Route::get('/peliculas/form', [PeliculaController::class, 'mostrar_formulario']);
-
+Route::get('/peliculas/show/{id}', [PeliculaController::class, 'show']);
 Route::post('/peliculas/store', [PeliculaController::class, 'store']);
+Route::put('/peliculas/update', [PeliculaController::class, 'update']);
+Route::delete('/peliculas/delete', [PeliculaController::class, 'delete']);
 
 // Route::get('/libro/create', [LibroController::class, 'create']);
 // Route::get('/libro/show', [LibroController::class, 'show']);
